77c4fc89edd0836bd4585f6d8c93490d12245702
Haitam el Attar <haitam@live.nl>
2017-12-25 20:15:48 +0100

    login and pages with roles works now

--- AngularClient/src/app/product/product.module.ts
+++ AngularClient/src/app/product/product.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsComponent } from './products/products.component';
+import { CreateProductComponent } from './create-product/create-product.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [ProductsComponent]
+  declarations: [ProductsComponent, CreateProductComponent]
 })
 export class ProductModule { }
